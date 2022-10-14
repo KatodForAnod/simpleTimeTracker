af1889cfbbb50c0fd3917c5cec792f23441d5a37
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-10-14 22:12:51 +0300

    db init and createtables func added

---
+++ pkg/db/sqlite/create_tables.go
@@ -0,0 +1,15 @@
+package sqlite
+
+const testTable = `CREATE TABLE if not exists notes(
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  name TEXT not null,
+  start datetime not null,
+  end datetime not null);`
+
+func (l *SqlLite) CreateTables() error {
+	_, err := l.conn.Exec(testTable)
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
--- pkg/db/sqlite/db.go
+++ pkg/db/sqlite/db.go
@@ -1,9 +1,37 @@
 package sqlite
 
 import (
-	_ "embed"
+	"database/sql"
+	"errors"
 	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"os"
 )
 
-//go:embed db.sqlite
-var sqlLiteFile []byte
+type SqlLite struct {
+	conn *sql.DB
+}
+
+func (l *SqlLite) InitDataBase() error {
+	path := "db.sqlite"
+
+	if _, err := os.Stat(path); err == nil {
+		// path/to/whatever exists
+	} else if errors.Is(err, os.ErrNotExist) {
+		_, err = os.Create(path)
+		if err != nil {
+			log.Fatalln(err)
+			return err
+		}
+	} else {
+		panic("error!")
+	}
+
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		panic(err)
+	}
+
+	l.conn = db
+	return nil
+}
