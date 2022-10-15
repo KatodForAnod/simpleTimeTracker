cf273c976570522df065e886ead4292c4880ea48
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-10-15 13:50:58 +0300

    req params struct added

--- pkg/models/dto.go
+++ pkg/models/dto.go
@@ -3,11 +3,19 @@ package models
 import "time"
 
 type Task struct {
-	Start time.Time
-	End   time.Time
-	Name  string
-	Id    int64
+	Start time.Time `json:"start"`
+	End   time.Time `json:"end"`
+	Name  string    `json:"name"`
+	Id    int64     `json:"id"`
 }
 
+type sortType string
+
+const (
+	ASC  sortType = "ASC"
+	DESC sortType = "DESC"
+)
+
 type ReqTaskParams struct {
+	Start time.Time `json:"start"`
 }
