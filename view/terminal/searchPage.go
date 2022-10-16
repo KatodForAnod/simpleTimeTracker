c705343e23d4abb837420899767deb25540ec3c4
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-10-16 17:36:05 +0300

    new struct searchPage

---
+++ view/terminal/searchPage.go
@@ -0,0 +1,103 @@
+package terminal
+
+import (
+	"fmt"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+	"simpleTimeTracker/pkg/models"
+)
+
+type searchPage struct {
+	searchBlock *tview.Form
+	tasksBlock  *tview.List
+	amountBlock *tview.TextView
+
+	tasks    []models.Task
+	currPage int
+}
+
+func (p *searchPage) createSearchPage(mainPage *tview.Application) (*tview.Flex, error) {
+	p.tasks = []models.Task{{Name: "task0"},
+		{Name: "task1"}, {Name: "task2"},
+		{Name: "task3"}, {Name: "task4"},
+		{Name: "task5"}, {Name: "task6"}}
+
+	p.tasksBlock = tview.NewList()
+	p.tasksBlock.SetTitle("Result " + HotKeysNamed[PagesHotKeys[SearchBlockTasks]]).SetBorder(true)
+	p.initSearchBlock()
+
+	p.amountBlock = tview.NewTextView()
+	p.amountBlock.SetBorderPadding(0, 0, 1, 1)
+	p.amountBlock.SetText("From - To: 2022-01-01 - 2022-02-02; Amount: 10min")
+	p.amountBlock.SetBorder(true)
+
+	flex := tview.NewFlex().
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(p.searchBlock, 4, 1, true).
+			AddItem(p.tasksBlock, 0, 3, false).
+			AddItem(p.amountBlock, 3, 3, false), 0, 2, false)
+
+	mainPage.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case PagesHotKeys[SearchBlock]:
+			mainPage.SetFocus(p.searchBlock)
+		case PagesHotKeys[SearchBlockTasks]:
+			mainPage.SetFocus(p.tasksBlock)
+		}
+		return event
+	})
+	return flex, nil
+}
+
+func (p *searchPage) initSearchBlock() error {
+	p.searchBlock = tview.NewForm()
+	taskName := tview.NewInputField().SetLabel("Task:").SetFieldWidth(20)
+	p.searchBlock.AddFormItem(taskName)
+
+	dropdown := tview.NewDropDown().
+		SetLabel("Duration: ").
+		SetOptions([]string{"Day", "Week", "Month"}, func(text string, index int) {
+			switch text {
+			case "Day":
+				p.updateTasks()
+			case "Week":
+				p.updateTasks()
+			case "Month":
+				p.updateTasks()
+			default:
+			}
+		})
+	p.searchBlock.AddFormItem(dropdown)
+	p.searchBlock.SetBorder(true)
+	p.searchBlock.SetTitle("Search " + HotKeysNamed[PagesHotKeys[SearchBlock]])
+	p.searchBlock.SetItemPadding(0)
+	p.searchBlock.SetBorderPadding(0, 0, 0, 0)
+	return nil
+}
+
+func (p *searchPage) updateTasks() {
+	if len(p.tasks) <= p.currPage*5 {
+		return
+	}
+
+	p.tasksBlock.Clear()
+	if p.currPage != 0 {
+		p.tasksBlock.AddItem("Back", "Return back ↑", 'b', func() {
+			p.currPage = p.currPage - 1
+			p.updateTasks()
+		})
+	}
+	for i := p.currPage * 5; i < (p.currPage+1)*5 && i < len(p.tasks); i++ {
+		start := p.tasks[i].Start.Format("2006-02-01")
+		end := p.tasks[i].End.Format("2006-02-01")
+		amount := "x" // temporary
+		secondaryText := fmt.Sprintf("Start: %s; End: %s, Amount: %s", start, end, amount)
+		p.tasksBlock.AddItem(p.tasks[i].Name, secondaryText, 'a', nil)
+	}
+	if (p.currPage+1)*5 < len(p.tasks) {
+		p.tasksBlock.AddItem("More", "Load more ↓", 'b', func() {
+			p.currPage = p.currPage + 1
+			p.updateTasks()
+		})
+	}
+}
