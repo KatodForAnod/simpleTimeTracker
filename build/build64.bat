111c5c4043d2588010aff1c86708abbc8d347577
KatodForAnod <nikita.makarchuck@yandex.by>
2022-11-24 11:28:30 +0300

    moved scripts

--- build/build64.bat
+++ build/build64.bat
@@ -5,3 +5,5 @@ rsrc.exe -manifest simpleTimeTracker.exe.manifest -o rcrs.syso
 windres -o res.syso simpleTimeTracker.rc
 
 go build -o simpleTimeTracker64.exe
+
+del rcrs.syso res.syso
--- build32.bat
+++
@@ -1,7 +0,0 @@
-go env -w GOOS=windows
-go env -w GOARCH=386
-
-rsrc.exe -arch 386 -manifest simpleTimeTracker.exe.manifest -o rcrs.syso
-windres -o res.syso simpleTimeTracker.rc -F pe-i386
-
-go build -o simpleTimeTracker32.exe
